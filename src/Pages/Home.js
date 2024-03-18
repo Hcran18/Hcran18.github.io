a9fbf75456a6c16d7029851b6c2783772e92cfe8
Hcran18 <hunter.crandall18@gmail.com>
2024-03-18 15:29:28 -0600

    very basic skeleton

---
+++ src/Pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div>
+            <h1>Welcome to my Developer Portfolio</h1>
+            <h2>Hi, I'm Hunter Crandall</h2>
+            <p>I am a passionate developer with expertise in web development.</p>
+            <p>Feel free to explore my projects and get in touch with me.</p>
+        </div>
+    );
+}
+
+export default Home;
