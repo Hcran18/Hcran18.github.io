b359c8e69f13184a49e3d996112e1336e334d542
Hcran18 <hunter.crandall18@gmail.com>
2024-03-20 20:56:57 -0600

    gradient and jobCard highlight on scroll

--- src/Components/JobCard.js
+++ src/Components/JobCard.js
@@ -2,17 +2,44 @@ import React from 'react';
 import './JobCardStyles.css';
 
 class JobCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            scrolled: false
+        };
+        this.handleScroll = this.handleScroll.bind(this);
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll);
+    }
+
+    handleScroll() {
+        const rect = this.cardRef.getBoundingClientRect();
+        if (rect.top < window.innerHeight / 2 && rect.bottom > window.innerHeight / 2 && !this.state.scrolled) {
+            this.setState({ scrolled: true });
+        } else if ((rect.top >= window.innerHeight / 2 || rect.bottom <= window.innerHeight / 2) && this.state.scrolled) {
+            this.setState({ scrolled: false });
+        }
+    }
+    
     render() {
         const { title, company, dates, descriptions } = this.props;
 
+        const { scrolled } = this.state;
+
         return (
-            <div className='job-card'>
+            <div className={`job-card ${scrolled ? 'scrolled' : ''}`} ref={ref => this.cardRef = ref}>
                 <h2 className='title'>{title}</h2>
                 <p className='company'>{company}</p>
                 <p className='location'>{dates}</p>
                 <ul className='descriptions'>
                     {descriptions.map((description, index) => (
-                    <li key={index} className='description'>{description}</li>
+                        <li key={index} className='description'>{description}</li>
                     ))}
                 </ul>
             </div>
